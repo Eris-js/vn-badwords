3325b61b2e406b50a62b7cdbd5f26fcde249a66f
Quang Sáng <quangsang.official@gmail.com>
2023-03-22 22:36:16 +0700

    Update thư viện

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 const { blacklist, badWords } = require('./lib/blacklist');
 module.exports = {
     blacklist,
     badWords
 }
-=======
-const { blackList, BadWords } = require('./lib/array')
-module.exports = {
-    blackList,
-    BadWords
-}
->>>>>>> e31c98791520881552863ac89b1d1121548d9039
+
